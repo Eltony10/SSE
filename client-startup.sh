3c3e8e47268d01ae90f6351e33bd1b7152159300
Antonio Cefalo <32614090+Eltony10@users.noreply.github.com>
2019-12-05 16:47:26 +0000

    Update client-startup.sh

--- client-startup.sh
+++ client-startup.sh
@@ -1,1 +1,12 @@
+key=`curl -s -H "Metadata-Flavor: Google"  \
+        "http://metadata.google.internal/computeMetadata/v1/instance/attributes/p"`
+ip=`curl -s -H "Metadata-Flavor: Google"  \
+        "http://metadata.google.internal/computeMetadata/v1/instance/attributes/ip"`
 
+sudo curl -sL https://deb.nodesource.com/setup_12.x | bash - 
+sudo apt-get install -y git nodejs
+
+    git clone https://github.com/portsoc/distributed-master-worker
+    cd distributed-master-worker
+    npm install
+    sudo npm run client $key $ip
