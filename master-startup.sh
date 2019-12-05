a91dfe63c5e75aa0d24540adb1323faf7aa73c3f
Antonio Cefalo <32614090+Eltony10@users.noreply.github.com>
2019-12-05 17:54:37 +0000

    Create master-startup.sh

---
+++ master-startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+key=`curl -s -H "Metadata-Flavor: Google"  \
+        "http://metadata.google.internal/computeMetadata/v1/instance/attributes/p"`
+
+sudo curl -sL https://deb.nodesource.com/setup_12.x | bash - 
+sudo apt-get install -y git nodejs
+    cd
+    git clone https://github.com/Eltony10/SSE
+    cd
+    git clone https://github.com/portsoc/distributed-master-worker
+    cd distributed-master-worker
+    npm install
+    sudo npm run server $key
+
+wait
+for i in `seq 1 $1`
+    do
+	    gcloud compute instances delete vm$i --delete-disks=all --zone=europe-west1-c
+    done
