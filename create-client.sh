bc74dcb21e3fe35ea363a5abe23e8cc332f2ea3f
Antonio Cefalo <32614090+Eltony10@users.noreply.github.com>
2019-12-05 17:00:59 +0000

    Update create-client.sh

--- create-client.sh
+++ create-client.sh
@@ -2,7 +2,7 @@ ip=`curl -s -H "Metadata-Flavor: Google"  \
  "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip"`
 
 key=`curl -s -H "Metadata-Flavor: Google"  \
-        "http://metadata.google.internal/computeMetadata/v1/instance/attributes/p"`
+  "http://metadata.google.internal/computeMetadata/v1/instance/attributes/p"`
 
 #creates the clients 
 for i in `seq 1 $1`
